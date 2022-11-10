6f0ff7d35708e4bacfb0c636a7a80274ec6e4a94
Kien_Nguyen1 <kien_nguyen1@epam.com>
2022-11-10 22:43:13 +0700

    initial commit

---
+++ src/main/java/com/epam/ld/module2/testing/Client.java
@@ -0,0 +1,35 @@
+package com.epam.ld.module2.testing;
+
+/**
+ * The type Client.
+ */
+public class Client {
+    private String addresses;
+    private String email;
+
+    /**
+     * Gets addresses.
+     *
+     * @return the addresses
+     */
+    public String getAddresses() {
+        return addresses;
+    }
+
+    /**
+     * Sets addresses.
+     *
+     * @param addresses the addresses
+     */
+    public void setAddresses(String addresses) {
+        this.addresses = addresses;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/epam/ld/module2/testing/template/Template.java
@@ -0,0 +1,10 @@
+package com.epam.ld.module2.testing.template;
+
+/**
+ * The type Template.
+ */
+public class Template {
+    private static final String PLACE_HOLDER = "#{}";
+    public static final String HELLO_FROM = "Hello from " + PLACE_HOLDER;
+    public static final String SEND_EMAIL = "Please send me an email to " + PLACE_HOLDER;
+}
